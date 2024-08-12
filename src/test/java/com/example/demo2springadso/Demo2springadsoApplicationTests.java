7e0328efe8bef1153a1c95e8540ee6f58c049b1d
jbedoyapformacion <84157796+jbedoyapformacion@users.noreply.github.com>
2024-08-12 02:32:12 -0500

    intento fallido

---
+++ src/test/java/com/example/demo2springadso/Demo2springadsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo2springadso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2springadsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
